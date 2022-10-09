8c05c6a3c08cb75fe6a439e7e555c57d5984a1eb
Chris Brownlee <76001262+FearsRedemption@users.noreply.github.com>
2022-10-09 18:55:02 -0500

    started code to attempt just loading via js method

---
+++ js/navbarLoader.js
@@ -0,0 +1,16 @@
+function navbarLoader() {
+    return (
+        <div className="hero">
+            <nav>
+                <h2 className="logo">Anime <span>Music</span> Showdown</h2>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <!--                <li><a href="#">About</a></li>-->
+                    <li><a href="countdown.html">Countdowns</a></li>
+                    <!--                <li><a href="#">Contact Us</a></li>-->
+                </ul>
+                <button type="button">Work In Progress</button>
+            </nav>
+        </div>
+    );
+}
